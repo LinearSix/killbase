a691508ae1f4f1f3238879662e54fc5ec6884a0f
David Nelson Murray <linearsix@gmail.com>
2018-09-20 21:01:14 -0500

    tried adding url user pass to staging in knexfile

--- knexfile.js
+++ knexfile.js
@@ -8,11 +8,11 @@ module.exports = {
   },
 
   staging: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: 'postgres://bhllimujmwwfug:48add9a09a226b25428486cc93eab6395232bfc4d343e66e1d9bda884f4730a3@ec2-54-243-147-162.compute-1.amazonaws.com:5432/de4kg3q8vdvcbb5',
+      user:     'linearsix@gmail.com',
+      password: '1HerokuIsEnough!'
     },
     pool: {
       min: 2,
