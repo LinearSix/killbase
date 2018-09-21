093359e2a6caac7ad2eeb0a3e91edcf0e1e4c017
David Nelson Murray <linearsix@gmail.com>
2018-09-21 09:10:50 -0500

    trying Mitali knexfile

--- knexfile.js
+++ knexfile.js
@@ -7,31 +7,27 @@ module.exports = {
     connection: 'postgres://localhost/killbase'
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      // port:     5432,
-      user:     '',
-      password: ''
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     // port:     5432,
+  //     user:     '',
+  //     password: ''
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+  
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'postgres://bhllimujmwwfug:48add9a09a226b25428486cc93eab6395232bfc4d343e66e1d9bda884f4730a3@ec2-54-243-147-162.compute-1.amazonaws.com:5432/de4kg3q8vdvcbb5',
-      // port:     5432,
-      user:     '',
-      password: ''
-    },
+    connection: process.env.DATABASE_URL + `?ssl=true`,
     pool: {
       min: 2,
       max: 10
@@ -40,5 +36,22 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   }
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'postgres://bhllimujmwwfug:48add9a09a226b25428486cc93eab6395232bfc4d343e66e1d9bda884f4730a3@ec2-54-243-147-162.compute-1.amazonaws.com:5432/de4kg3q8vdvcbb5',
+  //     // port:     5432,
+  //     user:     '',
+  //     password: ''
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
 
 };
