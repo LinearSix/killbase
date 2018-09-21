c80337f3f1abd86423ad501619baba10aa576b36
David Nelson Murray <linearsix@gmail.com>
2018-09-20 21:10:21 -0500

    added port 5433 to staging and user pass to all in knexfile

--- knexfile.js
+++ knexfile.js
@@ -8,9 +8,10 @@ module.exports = {
   },
 
   staging: {
-    client: 'pg',
+    client: 'postgresql',
     connection: {
-      database: 'postgres://bhllimujmwwfug:48add9a09a226b25428486cc93eab6395232bfc4d343e66e1d9bda884f4730a3@ec2-54-243-147-162.compute-1.amazonaws.com:5432/de4kg3q8vdvcbb5',
+      database: 'my_db',
+      port: 5433,
       user:     'linearsix@gmail.com',
       password: '1HerokuIsEnough!'
     },
@@ -24,7 +25,7 @@ module.exports = {
   },
 
   production: {
-    client: 'pg',
+    client: 'postgresql',
     connection: {
       database: 'postgres://bhllimujmwwfug:48add9a09a226b25428486cc93eab6395232bfc4d343e66e1d9bda884f4730a3@ec2-54-243-147-162.compute-1.amazonaws.com:5432/de4kg3q8vdvcbb5',
       user:     'linearsix@gmail.com',
