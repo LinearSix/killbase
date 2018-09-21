5ee18e91c172767dc131bb0c63fd575048b41759
David Nelson Murray <linearsix@gmail.com>
2018-09-20 21:14:33 -0500

    port 5432 in stage and prod - no user pass in either

--- knexfile.js
+++ knexfile.js
@@ -12,8 +12,8 @@ module.exports = {
     connection: {
       database: 'my_db',
       port:     5432,
-      user:     'linearsix@gmail.com',
-      password: '1HerokuIsEnough!'
+      user:     '',
+      password: ''
     },
     pool: {
       min: 2,
@@ -29,8 +29,8 @@ module.exports = {
     connection: {
       database: 'postgres://bhllimujmwwfug:48add9a09a226b25428486cc93eab6395232bfc4d343e66e1d9bda884f4730a3@ec2-54-243-147-162.compute-1.amazonaws.com:5432/de4kg3q8vdvcbb5',
       port:     5432,
-      user:     'linearsix@gmail.com',
-      password: '1HerokuIsEnough!'
+      user:     '',
+      password: ''
     },
     pool: {
       min: 2,
